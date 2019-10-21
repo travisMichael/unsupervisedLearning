a3a7a65774c8649ff8ec497ade8cac438922419e
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2019-10-20 19:32:05 -0500

    Added learning curve visuals

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import os
-import time
 import pickle
 from sklearn.metrics import f1_score
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import ShuffleSplit
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 
 def save_model(model, path, filename):
@@ -42,3 +46,53 @@ def train_and_time(model, X, y):
     end_time = time.time() - start_time
     print("training time", end_time)
     return model
+
+
+# np.linspace(.1, 1.0, 10)
+def write_learning_curve_stats(model, x, y, f):
+
+    cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+    print("Generating learning curve for model: ")
+    start_time = time.time()
+    train_sizes, train_scores, test_scores = learning_curve(model, x, y, cv=cv, train_sizes=[5, 10, 15, 20, 50, 100, 10000], scoring='accuracy')
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    # train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    # test_scores_std = np.std(test_scores, axis=1)
+    for i in range(len(train_sizes)):
+        f.write('%i\t%.3f\t%.3f\n' % (train_sizes[i], train_scores_mean[i], test_scores_mean[i]))
+
+    end_time = time.time() - start_time
+    f.write(str(end_time))
+
+    print("Learning curve finished for model: " + str(end_time))
+
+
+# def multiple_learning_curves_plot(model_list, x, y, colors, training_labels):
+#     number_of_models = len(model_list)
+#     cv = ShuffleSplit(n_splits=10, test_size=0.1, random_state=0)
+#     plt.figure()
+#     plt.legend(loc="best")
+#
+#     for i in range(number_of_models):
+#         print("Generating learning curve for model: " + str(i))
+#         start_time = time.time()
+#         model = model_list[i]
+#         curve_color = colors[i]
+#         label = training_labels[i]
+#
+#         train_sizes, train_scores, test_scores = learning_curve(model, x, y, cv=cv, n_jobs=2, train_sizes=np.linspace(.1, 1.0, 5), scoring='f1')
+#
+#         train_scores_mean = np.mean(train_scores, axis=1)
+#         train_scores_std = np.std(train_scores, axis=1)
+#         test_scores_mean = np.mean(test_scores, axis=1)
+#         test_scores_std = np.std(test_scores, axis=1)
+#
+#         plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1, color=curve_color)
+#         plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color=curve_color)
+#         plt.plot(train_sizes, train_scores_mean, color=curve_color, label=label)
+#         plt.plot(train_sizes, test_scores_mean, color=curve_color, linestyle='dashed')
+#         end_time = time.time() - start_time
+#         print("Learning curve finished for model: " + str(i) + " " + str(end_time))
+#     return plt
