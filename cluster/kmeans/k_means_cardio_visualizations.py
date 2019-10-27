ae6642bd3238ab2b0e03a9094213c0bb1b66990d
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2019-10-27 17:45:25 -0500

    k-means component plots

--- cluster/kmeans/k_means_cardio_visualizations.py
+++ cluster/kmeans/k_means_cardio_visualizations.py
@@ -9,6 +9,39 @@ from sklearn.metrics.cluster import homogeneity_score
 
 # https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_digits.html
 def run_k_means_on_loan_data(path):
+
+    data_set = 'loan'
+    x_train, y_train = load_data(path + 'data/' + data_set + '/train/')
+    # X, y = load_data(path + 'data/' + data_set + '/train/')
+
+    estimator = KMeans(n_clusters=15, random_state=0)
+    estimator.fit(x_train)
+
+    p = estimator.predict(x_train)
+
+    for i in range(15):
+        print(str(i) + " " + str(len(p[p==i])))
+
+    print("%.6f" % homogeneity_score(y_train, p))
+
+    centers = estimator.cluster_centers_
+
+    plt.scatter(x_train[:,0][p==2].ravel(), x_train[:,5][p==2].ravel(), alpha=.1, color='red')
+    plt.scatter(x_train[:,0][p==13].ravel(), x_train[:,5][p==13].ravel(), alpha=.1, color='blue')
+    # plt.scatter(x_train[:,2][p==2].ravel(), x_train[:,3][p==2].ravel(), alpha=.1, color='green')
+    plt.scatter(x_train[:,0][p==9].ravel(), x_train[:,5][p==9].ravel(), alpha=.1, color='yellow')
+    plt.xlabel('Loan Amount')
+    plt.ylabel('Annual Income')
+
+    if data_set is 'cardio':
+        plt.title('k-means on Cardiovascular Dataset')
+        plt.savefig("k-means_components_cardio.png")
+    else:
+        plt.title('k-means on Financial Loan Dataset')
+        plt.savefig("k-means_components_loan.png")
+
+
+def run_k_means_on_cardio_data(path):
     data_set = 'cardio'
     x_train, y_train = load_data(path + 'data/' + data_set + '/train/')
     # X, y = load_data(path + 'data/' + data_set + '/train/')
@@ -28,38 +61,22 @@ def run_k_means_on_loan_data(path):
     plt.scatter(x_train[:,2][p==0].ravel(), x_train[:,3][p==0].ravel(), alpha=.1, color='red')
     plt.scatter(x_train[:,2][p==13].ravel(), x_train[:,3][p==13].ravel(), alpha=.1, color='blue')
     # plt.scatter(x_train[:,2][p==2].ravel(), x_train[:,3][p==2].ravel(), alpha=.1, color='green')
-    plt.scatter(x_train[:,2][p==14].ravel(), x_train[:,3][p==14].ravel(), alpha=.1, color='yellow')
-    plt.scatter(x_train[:,2][p==2].ravel(), x_train[:,3][p==2].ravel(), alpha=.1, color='green')
+    plt.scatter(x_train[:,2][p==9].ravel(), x_train[:,3][p==9].ravel(), alpha=.1, color='yellow')
     plt.xlabel('Patient Height')
     plt.ylabel('Patient Weight')
-    # plt.ylim((0.0, 1.0))
-    plt.show()
 
-    plt.xlabel('Patient Height')
-    plt.ylabel('Patient Weight')
+    if data_set is 'cardio':
+        plt.title('k-means on Cardiovascular Dataset')
+        plt.savefig("k-means_components_cardio.png")
+    else:
+        plt.title('k-means on Financial Loan Dataset')
+        plt.savefig("k-means_components_loan.png")
+
 
     print(centers)
 
-    # f = open("loan_stats.txt","w+")
-    # bench_k_means("1", x_train, y_train, 1, f)
-    # bench_k_means("2", x_train, y_train, 2, f)
-    # bench_k_means("3", x_train, y_train, 3, f)
-    # bench_k_means("4", x_train, y_train, 4, f)
-    # bench_k_means("5", x_train, y_train, 5, f)
-    # bench_k_means("6", x_train, y_train, 6, f)
-    # bench_k_means("7", x_train, y_train, 7, f)
-    # bench_k_means("8", x_train, y_train, 8, f)
-    # bench_k_means("9", x_train, y_train, 9, f)
-    # bench_k_means("10", x_train, y_train, 10, f)
-    # bench_k_means("11", x_train, y_train, 11, f)
-    # bench_k_means("12", x_train, y_train, 12, f)
-    # bench_k_means("13", x_train, y_train, 13, f)
-    # bench_k_means("14", x_train, y_train, 14, f)
-    # bench_k_means("15", x_train, y_train, 15, f)
-    # f.close()
 
-
-def run_k_means_on_cardiovascular_data(path):
+def run_benchmark(path):
     data_set = 'cardio'
     x_train, y_train = load_data(path + 'data/' + data_set + '/train/')
     # X, y = load_data(path + 'data/' + data_set + '/train/')
@@ -110,5 +127,5 @@ def bench_k_means(name, data, labels, k, f):
 
 
 if __name__ == "__main__":
-    # train_neural_net('../', False)
     run_k_means_on_loan_data('../../')
+    run_k_means_on_cardio_data('../../')
